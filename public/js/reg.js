ac0744d54d60bab470dc03e867a1fc45e20f3e68
Anohkin B.S. <bogdan.an.2009@gmail.com>
2025-01-05 19:54:09 +0300

    Добавлен вход - login (ОФ - Основной функционал) Объявлены session (beta) У Alert есть callback! (UI)

--- public/js/reg.js
+++ public/js/reg.js
@@ -9,34 +9,34 @@
 //   }
 //* не уверен, что проверка необходима на пароль, ввиду того, что там могут быть разные символы
 
-function reg(){
+function reg() {
     err = false;
     r = document.getElementById("col_reg").children;
-    
-    if ((/^[а-яА-Я]+$/.test(r[2].value)) && (/^[а-яА-Я]+$/.test(r[3].value))){
+
+    if ((/^[а-яА-Я]+$/.test(r[2].value)) && (/^[а-яА-Я]+$/.test(r[3].value))) {
         //имя фамилия норм
-    }else{
+    } else {
         Alert('Ваше ФИ неверно');
         err = true;
     }
 
     const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-    if (EMAIL_REGEXP.test(r[4].value)){
+    if (EMAIL_REGEXP.test(r[4].value)) {
         //почта в порядке
-    }else{
+    } else {
         Alert('Неверные данные почты');
         err = true;
     }
 
-    if(r[5].value == r[6].value){
+    if (r[5].value == r[6].value) {
         //пароли совпадают
-    }else{
+    } else {
         Alert('Пароли не совпадают');
         err = true;
     }
 
 
-    if(!err){
+    if (!err) {
         data = {
             name: r[2].value,
             surname: r[3].value,
@@ -49,3 +49,44 @@ function reg(){
     }
 }
 
+function log() {
+
+    err = false;
+
+    l = document.getElementById("col_log").children;
+
+    if (/^[а-яА-Я]+$/.test(l[2].value)) {
+        //имя норм
+    } else {
+        Alert('Ваше Имя неверно');
+        err = true;
+    }
+
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    if (EMAIL_REGEXP.test(l[3].value)) {
+        //почта в порядке
+    } else {
+        Alert('Неверные данные почты');
+        err = true;
+    }
+
+    if (!err) {
+        data = {
+            name: l[2].value,
+            mail: l[3].value,
+            pass: l[4].value,
+        }
+        POST("/log", data, (text) => {
+            if(text != "OK"){
+                Alert(text);
+            }
+            Alert("Успешно!", () => {window.location.replace("/")})
+            console.log(text);
+        })
+    }
+}
+
+
+function islogged() {
+
+}
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
+
 const app = express();
 const port = 3000;
+var session = require('express-session');
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("db.db");
 
@@ -9,6 +11,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ dest: "upload/", extended: false })); 
 //* заставляет читать body, изначальная библиотека Node.js
+//* "Модуль body-parser необходим для корректной обработки передаваемых в теле данных."
 
 app.set("view engine", "ejs"); 
 
@@ -23,14 +26,21 @@ app.use(express.static("public"));
 // });
 //? куки на будущее
 
-const { createHmac } = await import('node:crypto');
+app.use(session({ secret: 'i_still%full_of&gold', cookie: { maxAge: 60000 } }))
+
 //зачем хэш если пароль изначально можно перехватить? только если бд взломают, но зачем?
 //сделаю хэш, чтобы в будущем разместить бд где-то (что понижает уровень безопастности)
-const myhash = createHmac('sha256', 'iAmGold');
+//* из-за неразумности идея отложена
+// const { createHmac } = await import('node:crypto');
+// const myhash = createHmac('sha256', 'iAmGold');
 
 //! БЛОК РЕНДЕРА
-app.get("/", function (req, res) {
+app.get("/reg", function (req, res) {
   res.render("pages/reg");
+});
+
+app.get("/", function (req, res) {
+  res.render("pages/main");
 });
 
 
@@ -50,6 +60,43 @@ app.post("/reg", (req, res) => {
   );
 });
 
+
+app.post('/log', (req, res) => {
+
+  db.all("SELECT * FROM users WHERE name=? AND mail=? AND pass=?", [req.body.name, req.body.mail, req.body.pass], (err, row) => {
+    if (err) {
+      res.send("Внутрянняя ошибка");
+    } else {
+      if (row[0]) {
+
+        req.session.name = req.body.name
+        req.session.uid = row[0].id
+
+        res.send("OK");
+
+      } else {
+        res.send("Несуществующие данные");
+      }
+    }
+  })
+})
+
+app.get("/getUserInfo", (req, res) => {
+  if (!req.session.uid) {
+      res.send("NO");
+      return
+    }
+
+  db.all("SELECT * FROM users WHERE uid = ?", [req.session.uid], (err,row)=>{
+      if(err){
+          res.send("Внутряння ошибка")
+
+      }else{
+        res.send(row);
+      }
+      
+  })
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
